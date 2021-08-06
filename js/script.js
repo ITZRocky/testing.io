d235a8d4ba94330c0c06ad8e486649fbaf0f4580
ROHITH V <75740039+ITZRocky@users.noreply.github.com>
2021-08-06 17:30:25 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -96,6 +96,22 @@ tabsContainer.addEventListener("click", (e) =>{
     portfolioItem.querySelector(".portfolio-item-details").innerHTML;
  }
  
+//smooth scrolling
+
+    $('a[href*="#"]').on('click',function(e){
+
+      e.preventDefault();
+
+      $('html, body').animate({
+
+        scrollTop : $($(this).attr('href')).offset().top,
+
+      },
+      500,
+      'linear'
+      )
+
+    })
 
 
 
